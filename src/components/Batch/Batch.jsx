2093682f952e1b921234340a795bca8848c8861a
Vlad Melnik <vmelniki2828@gmail.com>
2024-05-17 11:57:03 +0300

    edit

--- src/components/Batch/Batch.jsx
+++ src/components/Batch/Batch.jsx
@@ -1,24 +1,24 @@
-import OpenAI from 'openai';
-// import asd from '../asd.jsonl'
-// import fs from 'fs-extra'
+// import OpenAI from 'openai';
+// // import asd from '../asd.jsonl'
+// // import fs from 'fs-extra'
 
-const Batch = () => {
-//     const openai = new OpenAI({
-//         apiKey: process.env.REACT_APP_API_AI_KEY,
-//         dangerouslyAllowBrowser: true,
-//       });
-//   async function main() {
+// const Batch = () => {
+// //     const openai = new OpenAI({
+// //         apiKey: process.env.REACT_APP_API_AI_KEY,
+// //         dangerouslyAllowBrowser: true,
+// //       });
+// //   async function main() {
 
-//     const file = await openai.files.create({
-//         file: fs.createReadStream("batchinput.jsonl"),
-//         purpose: "batch",
-//       });
+// //     const file = await openai.files.create({
+// //         file: fs.createReadStream("batchinput.jsonl"),
+// //         purpose: "batch",
+// //       });
 
-//   }
+// //   }
 
-//   main();
+// //   main();
 
-  return <></>;
-};
+//   return <></>;
+// };
 
-export default Batch;
+// export default Batch;
